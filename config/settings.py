b5ab08dd7213b40a0b2644abd771a555d6cbb2ad
Sudeepto_bhakat <118655609+SudeeptoBhakat@users.noreply.github.com>
2026-03-02 16:31:49 +0530

    Add AccountMiddleware to MIDDLEWARE settings

--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-
+    "allauth.account.middleware.AccountMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
